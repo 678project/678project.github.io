6524632ef23f48393c2552d737194f6f7fb7f43a
678project <1070703@lwsd.org>
2025-05-13 17:48:53 -0700

    Save1 block.js
    WIP

--- block.js
+++ block.js
@@ -1,6 +1,45 @@
 // Blocks website access from certain time ranges
+let day = '';
 const currentDate = Date();
+if (currentDate.includes('Wen')) {
+  day = '3';
+} else if (currentDate.includes('Mon') || currentDate.includes('Fri')) {
+  day = '1';
+} else if (currentDate.includes('Tue') || currentDate.includes('Thu')) {
+  day = '2';
+}
+console.log(day);
 const currentDateArray = currentDate.split(' ');
 const currentTime = currentDateArray[4];
 const currentTimeInt = currentTime.replaceAll(':', '');
 console.log(`Time: ${currentTimeInt}`);
+/* Mon and Fri
+  LStart:
+  105000
+  LEnd:
+  122000
+  SStart:
+  
+  SEnd:
+  142000
+*/
+/* Tue and Thu
+  LStart:
+  110800
+  LEnd:
+  123800
+
+  SEnd:
+  142000
+*/
+/* Wen
+  LStart:
+  ??
+  LEnd:
+  ??
+  
+  SEnd:
+  125000
+*/
+
+//if (currentTimeInt >
